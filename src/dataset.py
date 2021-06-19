54c70606cfdb197bb0c0601f8264017df536a58d
jay-in-git <moom60225@gmail.com>
2021-06-19 23:16:57 +0800

    Return query length

--- src/dataset.py
+++ src/dataset.py
@@ -9,11 +9,21 @@ import gensim.downloader as api
 
 
 class DRMMDataset(Dataset):
-    def __init__(self, qrels_file, topics_file, docs_dir, word_model=None, mode='train', use_topic=["title", "description"]):
-        with open(qrels_file) as f_qrel:
-            self.qrels = json.load(f_qrel)
+    def __init__(self, qrels_file, topics_file, docs_dir, word_model=None, mode='train', use_tag=["title", "description"]):
         self.pos_docs = dict()
         self.neg_docs = dict()
+        self.mode = mode
+        self.use_tag = use_tag
+        self.docs_dir = Path(docs_dir)
+        
+        if word_model is None:
+            word_model = api.load('word2vec-google-news-300')
+        self.word2id = word_model.key_to_index
+
+        with open(qrels_file) as f_qrel:
+            self.qrels = json.load(f_qrel)
+        with open(topics_file) as f_topic:
+            self.topics = json.load(f_topic)
 
         for qid in self.qrels:
             self.pos_docs[qid] = list()
@@ -24,10 +34,6 @@ class DRMMDataset(Dataset):
                 else:
                     self.neg_docs[qid].append(doc)
 
-        self.mode = mode
-
-        with open(topics_file) as f_topic:
-            self.topics = json.load(f_topic)
 
         total_qids = list(self.topics.keys())
         total_qids = np.array([int(qid) for qid in total_qids])
@@ -35,24 +41,16 @@ class DRMMDataset(Dataset):
             indexs = list(set(range(len(total_qids))) - set((range(0, len(total_qids), 5))))
         else:
             indexs = list(range(0, len(total_qids), 5))
+
         self.qids = total_qids[indexs]
-
-        self.use_topic = use_topic
-        self.docs_dir = Path(docs_dir)
-
-        if word_model is None:
-            print('Loading word2vec model...')
-            word_model = api.load('word2vec-google-news-300')
-        self.word2id = word_model.key_to_index
 
     def __len__(self):
         return len(self.qids)
 
     def __getitem__(self, index):
         qid = str(self.qids[index])
-        # print(f'=========={qid}===========')
         query = ""
-        for tag in self.use_topic:
+        for tag in self.use_tag:
             query += self.topics[qid][tag]
             query += ' '
         query = self.convertSentence(query)
@@ -61,10 +59,8 @@ class DRMMDataset(Dataset):
         neg_doc = random.choice(self.neg_docs[qid])
 
         with open(self.docs_dir / pos_doc) as f_pos_doc:
-            # print(f'=============={pos_doc}==============')
             pos_doc_content = self.convertSentence(f_pos_doc.read())
         with open(self.docs_dir / neg_doc) as f_neg_doc:
-            # print(f'=============={neg_doc}==============')
             neg_doc_content = self.convertSentence(f_neg_doc.read())
         
         return query, pos_doc_content, neg_doc_content
@@ -74,11 +70,9 @@ class DRMMDataset(Dataset):
             # remove puctuation
             word = word.translate(str.maketrans(string.punctuation, ' '*len(string.punctuation))).strip().split()
             for w in word:
-                w = w.lower()
                 try:
                     vec.append(self.word2id[w])
                 except:
-                    # print(f'{w} fail')
                     continue
         return torch.tensor(vec)
 
@@ -88,7 +82,8 @@ def collate_batch(batch):
     q = torch.reshape(torch.nn.utils.rnn.pad_sequence(q), (batch_size, -1))
     p = torch.reshape(torch.nn.utils.rnn.pad_sequence(p), (batch_size, -1))
     n = torch.reshape(torch.nn.utils.rnn.pad_sequence(n), (batch_size, -1))
-    return q, p, n
+    l = torch.tensor([q_vec.shape[0] for q_vec in q])
+    return q, p, n, l
 
 if __name__ == '__main__':
     import argparse
@@ -98,10 +93,7 @@ if __name__ == '__main__':
     parser.add_argument('docs_dir', type=str, help="Doc dir in json format")
     argvs = parser.parse_args()
     test = DRMMDataset(argvs.qrels_file, argvs.topics_file, argvs.docs_dir)
-    # print('Load done')
-    # a, b, c = test[0]
-    # print(b)
     loader = DataLoader(test, batch_size=2, shuffle=False, collate_fn=collate_batch)
-    for q, p, n in loader:
-        print(q, p, n, sep='\n')
+    for q, p, n, l in loader:
+        print(q, p, n, l, sep='\n')
         break
