1f01f3add716e591c2c6d7b25a57bd5037d3e2a1
jay-in-git <moom60225@gmail.com>
2021-06-20 18:31:04 +0800

    Add idf in generated data

--- src/dataset.py
+++ src/dataset.py
@@ -6,16 +6,17 @@ import string
 import random
 import numpy as np
 import gensim.downloader as api
+from rank_bm25 import BM25Okapi
+
 
 
 class DRMMDataset(Dataset):
-    def __init__(self, qrels_file, topics_file, docs_dir, word_model=None, mode='train', use_tag=["title", "description"]):
+    def __init__(self, qrels_file, topics_file, docs_file, word_model=None, mode='train', use_tag=["title", "description"]):
         self.pos_docs = dict()
         self.neg_docs = dict()
         self.mode = mode
         self.use_tag = use_tag
-        self.docs_dir = Path(docs_dir)
-
+        self.translator = str.maketrans(string.punctuation, ' '*len(string.punctuation))
         if word_model is None:
             word_model = api.load('glove-twitter-25')
         self.word2id = word_model.key_to_index
@@ -24,6 +25,18 @@ class DRMMDataset(Dataset):
             self.qrels = json.load(f_qrel)
         with open(topics_file) as f_topic:
             self.topics = json.load(f_topic)
+        with open(docs_file) as f_docs:
+            self.docs = json.load(f_docs)
+
+        # Use bm25 to compute idf
+        print('Computing IDF...', end='')
+        corpus = list()
+        for doc in self.docs:
+            corpus_tokens = self.docs[doc].translate(self.translator).strip().lower().split()
+            corpus.append([token.strip() for token in corpus_tokens if token.strip() != ''])
+        self.idf = BM25Okapi(corpus).idf
+        print('Done')
+        del corpus, corpus_tokens
 
         total_qids = list(self.qrels.keys())
         total_qids = np.array([int(qid) for qid in total_qids])
@@ -53,48 +66,50 @@ class DRMMDataset(Dataset):
         for tag in self.use_tag:
             query += self.topics[qid][tag]
             query += ' '
-        query = self.convert_sentence(query.lower())
+        query, q_idf = self.convert_sentence(query.lower())
 
         pos_doc = random.choice(self.pos_docs[qid])
         neg_doc = random.choice(self.neg_docs[qid])
 
-        with open(self.docs_dir / pos_doc) as f_pos_doc:
-            pos_doc_content = self.convert_sentence(f_pos_doc.read().lower())
-        with open(self.docs_dir / neg_doc) as f_neg_doc:
-            neg_doc_content = self.convert_sentence(f_neg_doc.read().lower())
+        pos_doc_content, _ = self.convert_sentence(self.docs[pos_doc].lower())
+        neg_doc_content, _ = self.convert_sentence(self.docs[neg_doc].lower())
         
-        return query, pos_doc_content, neg_doc_content
+        return query, pos_doc_content, neg_doc_content, q_idf
 
     def convert_sentence(self, s):
         vec = list()
+        w_idf = list()
         for word in s.split():
             # remove puctuation
             word = word.translate(str.maketrans(string.punctuation, ' '*len(string.punctuation))).strip().split()
             for w in word:
                 try:
                     vec.append(self.word2id[w])
+                    w_idf.append(self.idf[w])
                 except:
                     continue
-        return torch.tensor(vec)
+        return torch.tensor(vec), torch.tensor(w_idf)
 
 def collate_batch(batch):
-    q, p, n = zip(*batch)
+    q, p, n, idf = zip(*batch)
     batch_size = len(batch)
     l = torch.tensor([q_vec.shape[0] for q_vec in q])
     q = torch.reshape(torch.nn.utils.rnn.pad_sequence(q), (batch_size, -1))
     p = torch.reshape(torch.nn.utils.rnn.pad_sequence(p), (batch_size, -1))
     n = torch.reshape(torch.nn.utils.rnn.pad_sequence(n), (batch_size, -1))
-    return q, p, n, l
+    idf = torch.reshape(torch.nn.utils.rnn.pad_sequence(idf), (batch_size, -1))
+    return q, p, n, l, idf
 
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser(description='dataset')
     parser.add_argument('qrels_file', type=str, help="Qrel file in json format")
     parser.add_argument('topics_file', type=str, help="Topic file in json format")
-    parser.add_argument('docs_dir', type=str, help="Doc dir")
+    parser.add_argument('docs_file', type=str, help="Clean documents in json format")
     argvs = parser.parse_args()
-    test = DRMMDataset(argvs.qrels_file, argvs.topics_file, argvs.docs_dir)
+    test = DRMMDataset(argvs.qrels_file, argvs.topics_file, argvs.docs_file)
+    print('Load data done')
     loader = DataLoader(test, batch_size=2, shuffle=False, collate_fn=collate_batch)
-    for q, p, n, l in loader:
-        print(l, sep='\n')
+    for q, p, n, l, idf in loader:
+        print(l, idf,  sep='\n')
         input()
